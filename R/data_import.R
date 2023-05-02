10ad9e447cc69dbf1066da7a39d4d395f485d071
Vivien Lee <vivienlvv@gmail.com>
2023-05-02 13:23:38 -0500

    Saving data object

--- R/data_import.R
+++ R/data_import.R
@@ -6,8 +6,8 @@ library(tidyverse)
 # Data Import and Cleaning
 
 ## Importing data
-employee_tbl = read_delim("../data/dataset.csv", delim = "+", col_names = TRUE)
-review_tbl =  read_delim("../data/satisfaction_reviews.csv", delim = ".", col_names = c("pros", "cons", "employeeID"))
+employee_tbl = read_delim("../data/dataset.csv", delim = "+", col_names = TRUE, show_col_types = FALSE)
+review_tbl =  read_delim("../data/satisfaction_reviews.csv", delim = ".", col_names = c("pros", "cons", "employeeID"), show_col_types = FALSE)
 
 ## Merging the two datasets by employeeID 
 combined_tbl = employee_tbl %>%
@@ -19,4 +19,3 @@ combined_tbl = employee_tbl %>%
 
 ## Saving joined data into an object
 saveRDS(combined_tbl, "../data/combined_tbl.RDS")
-  
